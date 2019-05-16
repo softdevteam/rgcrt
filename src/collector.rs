7d61c96829b4cd5e396252e72add799b30d143cb
Jacob Hughes <jacob.hughes@kcl.ac.uk>
2019-05-16 21:51:18 +0100

    Flesh out collector API
    This introduces a basic API for mutator threads to interact with the GC
    runtime. Collection is managed through a thread local `Collector`
    object. A lot of the functionality is unimplemented but this presents a
    high-level overview of how the structure of the GC will look.

---
+++ src/collector.rs
@@ -0,0 +1,77 @@
+use std::{
+    alloc::{alloc, Layout},
+    cell::{Cell, UnsafeCell},
+    collections::HashMap,
+    path::Path
+};
+
+use crate::{
+    safepoints::{ReturnAddress, SafepointRoots},
+    Scan, GcErr
+};
+
+/// The size of the heap in bytes
+const HSIZE: usize = 1024;
+
+/// The byte alignment of the heap
+const HALIGN: usize = 4;
+
+pub(crate) struct Collector {
+    hptr: Cell<*mut usize>,
+    hstart: Cell<usize>,
+    hend: Cell<usize>,
+
+    collect_next: Cell<bool>,
+
+    roots: UnsafeCell<Option<HashMap<ReturnAddress, SafepointRoots>>>
+}
+
+impl Collector {
+    pub(crate) fn new() -> Self {
+        Collector {
+            hptr: Cell::new(0 as *mut usize),
+            hstart: Cell::new(0),
+            hend: Cell::new(0),
+
+            collect_next: Cell::new(false),
+            roots: UnsafeCell::new(None)
+        }
+    }
+
+    #[inline]
+    pub fn collect_next(&self) {
+        self.collect_next.set(true);
+    }
+
+    #[inline]
+    pub fn should_collect(&self) -> bool {
+        self.collect_next.get()
+    }
+
+    pub fn mk_heap(&self) {
+        let layout = Layout::from_size_align(HSIZE, HALIGN).unwrap();
+        let ptr = unsafe { alloc(layout) as *mut usize };
+
+        if ptr.is_null() {
+            panic!("Can't allocate memory.");
+        }
+
+        self.hptr.set(ptr);
+        self.hstart.set(ptr as usize);
+        self.hend.set(ptr as usize + HSIZE);
+    }
+
+    pub fn mk_root_table<P: AsRef<Path>>(&self, path: P) {
+        unimplemented!()
+    }
+
+    // Perform the actual garbage collection. We use the name `reclaim` to
+    // disambiguate from Rust's notion of `collect` on iterators.
+    pub(crate) fn reclaim(&self) {
+        unimplemented!()
+    }
+
+    pub(crate) fn alloc_obj<T: Scan>(&self, object: T) -> Result<*mut T, GcErr> {
+        unimplemented!()
+    }
+}
